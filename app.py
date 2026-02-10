fb6e7b1ecc057bf26a748c6bf9cd318e6765e161
Venkat Golakoti <venkat.g93@gmail.com>
2026-02-10 20:23:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 @app.route('/api/hello')
 def hello_world():
     """Returns Hello, EDP!"""
-    return 'Hello, EDP!'
+    return "Hello, KubeRocketCI!"
 
+# Readiness probe – controls traffic
+@app.route('/api/readiness')
+def readiness():
+    return jsonify(status="readiness-ok"), 200
+
+# Liveness probe – controls restart
+@app.route('/api/liveness')
+def liveness():
+    return jsonify(status="liveness-ok"), 200
 
 if __name__ == '__main__':
     app.run()
